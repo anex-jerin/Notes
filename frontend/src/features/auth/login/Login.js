298eb8a0397ad2163292d259c0336ed9b42cb944
anex <anexcruise@gmail.com>
2023-02-20 19:53:00 +0530

    eighth

--- frontend/src/features/auth/login/Login.js
+++ frontend/src/features/auth/login/Login.js
@@ -4,7 +4,16 @@ import './login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='main'>
+      <div className="l-section">
+        <h1>Login</h1>
+        <form>
+          <input type="text" id='email' name='email' placeholder='Username'/>
+          <input type="text" id='password' name='password' placeholder='Password'/>
+          <button type='submit' >LOGIN</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
